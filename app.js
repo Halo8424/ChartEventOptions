da58f641566e395cf3cb5661e9f7c0f17f52b157
Richard.C <r.conde@live.com>
2022-07-27 22:29:34 -0400

    desktop fix on maps

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
-document.ready(function(){
-    
-  function mapsSelector() {
-  if (
-    /* if we're on iOS, open in Apple Maps */
-    navigator.platform.indexOf("iPhone") != -1 ||
-    navigator.platform.indexOf("iPod") != -1 ||
-    navigator.platform.indexOf("iPad") != -1
-  )
-    //   window.open("maps://maps.google.com/maps?daddr=30.267586307779652,long&amp;ll=");
-    window.open(
-      "maps://maps.apple.com/?address=606%20Trinity%20St,%20Austin,%20TX%20%2078701,%20United%20States&ll=30.267350,-97.739415&q=606%20Trinity%20St&_ext=EiYpnEQpqEpDPkAx/E8oyKdvWMA5mO90YJdFPkBBmAVEX/1uWMBQAw%3D%3D&t=m"
-    );
-  /* else use Google */ else
-    window.open(
-      "https://maps.google.com/maps?address=606%20Trinity%20St,%20Austin,%20TX%20%2078701,%20United%20States&ll=30.267350,-97.739415&q=606%20Trinity%20St&_ext=EiYpnEQpqEpDPkAx/E8oyKdvWMA5mO90YJdFPkBBmAVEX/1uWMBQAw%3D%3D&t=m"
-    );
-}  
-});
+function mapsSelector() {
+    if (
+        /* if we're on iOS, open in Apple Maps */
+        navigator.platform.indexOf("iPhone") != -1 ||
+        navigator.platform.indexOf("iPod") != -1 ||
+        navigator.platform.indexOf("iPad") != -1
+    )
+        
+        window.open(
+        "maps://maps.apple.com/?address=606%20Trinity%20St,%20Austin,%20TX%20%2078701,%20United%20States&ll=30.267350,-97.739415&q=606%20Trinity%20St&_ext=EiYpnEQpqEpDPkAx/E8oyKdvWMA5mO90YJdFPkBBmAVEX/1uWMBQAw%3D%3D&t=m"
+        );
+    /* else use Google */ else
+        window.open(
+        "https://maps.google.com/maps?address=606%20Trinity%20St,%20Austin,%20TX%20%2078701,%20United%20States&ll=30.267350,-97.739415&q=606%20Trinity%20St&_ext=EiYpnEQpqEpDPkAx/E8oyKdvWMA5mO90YJdFPkBBmAVEX/1uWMBQAw%3D%3D&t=m"
+        );
+    }  
 
 
 
-//   30.267586307779652, -97.73941301581195
